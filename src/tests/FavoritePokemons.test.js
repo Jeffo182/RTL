dc44ee87f685ea5c59916b18813bc3831bb0c0c2
Jefferson Felix <jefferson.felix182@gmail.com>
2022-09-19 16:30:35 -0300

    Req 3 - ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testando pagina FavoritePokemons', () => {
+  test('Titulo -FavoritePokemons Pokédex-', () => {
+    render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
+
+    const FavoritePokemonsText = screen.getByRole('heading', {
+      name: /Favorite pokémons/i,
+      level: 2,
+    });
+    expect(FavoritePokemonsText).toBeInTheDocument();
+  });
+  test('paragrafo 1 -No favorite pokemon found-', () => {
+    render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
+
+    const FavoritePokemonsText = screen.getByText(/No favorite pokemon found/i);
+    expect(FavoritePokemonsText).toBeInTheDocument();
+  });
+});
+
+// ate aqui
