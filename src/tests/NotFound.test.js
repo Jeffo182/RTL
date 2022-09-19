1246a70ca50364cff9c5b0d08bd5a63e152cbbea
Jefferson Felix <jefferson.felix182@gmail.com>
2022-09-19 16:34:54 -0300

    Req 4 - ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import NotFound from '../pages/NotFound';
+
+describe('Testando pagina NotFound', () => {
+  test('Titulo -NotFound-', () => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+
+    const NotFoundText = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(NotFoundText).toBeInTheDocument();
+  });
+  test('img pikachu chorando-', () => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+
+    const imgAbout = screen.getByRole('img', {
+      name: /Pikachu crying because the page requested was not found/i,
+    });
+    expect(imgAbout).toBeInTheDocument();
+    expect(imgAbout).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
+
+// ate aqui
