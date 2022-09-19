3abb493ab641a37b9ff7b0139006dcf945b5797c
Jefferson Felix <jefferson.felix182@gmail.com>
2022-09-19 19:38:16 -0300

    Req 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,68 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import userEvent from '@testing-library/user-event';
+import pokemons from '../data';
+import Pokedex from '../pages/Pokedex';
+
+describe('Testando pagina Pokedex', () => {
+  test('Titulo -Pokedex-', () => {
+    const pokemonsFav = {
+      4: false,
+      10: false,
+      23: false,
+      25: false,
+      65: false,
+      78: false,
+      143: false,
+      148: false,
+      151: false,
+    };
+    render(
+      <MemoryRouter>
+        <Pokedex
+          pokemons={ pokemons }
+          isPokemonFavoriteById={ pokemonsFav }
+        />
+      </MemoryRouter>,
+    );
+    const PokedexText = screen.getByRole('heading', {
+      name: /Encountered pokémons/i,
+      level: 2,
+    });
+    expect(PokedexText).toBeInTheDocument();
+  });
+  test('Botao proximo pokemon -Pokedex-', async () => {
+    const pokemonsFav = {
+      4: false,
+      10: false,
+      23: false,
+      25: false,
+      65: false,
+      78: false,
+      143: false,
+      148: false,
+      151: false,
+    };
+    render(
+      <MemoryRouter>
+        <Pokedex
+          pokemons={ pokemons }
+          isPokemonFavoriteById={ pokemonsFav }
+        />
+      </MemoryRouter>,
+    );
+    const buttonEl = screen.getByRole('button', { name: /Próximo pokémon/i });
+    expect(buttonEl).toBeInTheDocument();
+    userEvent.click(buttonEl);
+    const aboutText = await screen.getByText(/Charmander/i);
+    expect(aboutText).toBeInTheDocument();
+    const typePoke = await screen.getByTestId('pokemon-type', {
+      type: /Fire/i,
+    });
+    expect(typePoke).toBeInTheDocument();
+  });
+});
+
+// ate aqui
